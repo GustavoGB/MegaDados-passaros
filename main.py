d6f950680f2a2091e64fcb98ddeb1ec04d762fb5
unknown <gustavo.gobetti98@gmail.com>
2019-10-19 16:02:49 -0300

    Passando tudo para REST

--- main.py
+++ main.py
@@ -1,22 +1,57 @@
 from fastapi import FastAPI
 from pydantic import BaseModel 
+import pymysql
 
 app = FastAPI()
 
 
-class Item(BaseModel):
-	name : str
-	price : int
-	is_offer : bool = None 
+@app.post("/usuarios/{usuario_id}")
+def adiciona_usuario(conn, nome, email, cidade):
+    with conn.cursor() as cursor:
+        try:
+            cursor.execute('INSERT INTO usuario (nome, EMAIL, cidade) VALUES (%s, %s, %s)', (nome, email, cidade))
+        except pymysql.err.IntegrityError as e:
+            raise ValueError(f'Não posso inserir {nome} na tabela perigo')
 
-@app.get("/")
-async def read_root():
-	return{"Hello":"World"}
 
-@app.get("/items/{item_id}")
-async def read_item(item_id:int, q: str = None):
-	return {"item_id":item_id,"q":q}
+@app.get("/usuarios/{usuario_id}")
+def acha_usuario(conn, nome):
+    with conn.cursor() as cursor:
+        cursor.execute('SELECT id_usuario FROM usuario WHERE nome = %s', (nome))
+        res = cursor.fetchone()
+        if res:
+            return res[0]
+        else:
+            return None		
 
-@app.put("/items/{item_id}")
-def update_item(item_id:int, item: Item):
-	return {"item_name":item.name, "item_id": item_id}
+@app.put("/usuarios/{usuario_id}")
+def muda_nome_usuario(conn, id_usuario, novo_nome):
+    with conn.cursor() as cursor:
+        try:
+            cursor.execute('UPDATE usuario SET nome=%s where id_usuario=%s', (novo_nome, id_usuario))
+        except pymysql.err.IntegrityError as e:
+            raise ValueError(f'Não posso alterar nome do id {id} para {novo_nome} na tabela usuario')
+
+@app.delete("/usuario/{usuario_id}")
+def remove_usuario(conn, id_usuario):
+    with conn.cursor() as cursor:
+        try:
+        	cursor.execute('UPDATE usuario SET ativo = False WHERE id_usuario=%s', (id_usuario))
+        except pymysql.err.IntegrityError as e:
+            raise ValueError(f'Não posso remover usuario de id {id} na tabela usuario')
+@app.get("/usuario/{usuario_id}")
+def lista_usuarios(conn):
+    with conn.cursor() as cursor:
+        cursor.execute('SELECT id_usuario from usuario')
+        res = cursor.fetchall()
+        usuarios = tuple(x[0] for x in res)
+        return usuarios
+
+def checa_ativo(conn, id_usuario):
+    with conn.cursor() as cursor:
+        cursor.execute('SELECT ativo FROM usuario WHERE id_usuario = %s', (id_usuario))
+        res = cursor.fetchone()
+        if res:
+            return res[0] #Retorna se está 1 ou 0
+        else:
+            return -1
