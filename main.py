b2e5051f8d85f25dd8f5d7a396dea9d339919479
GustavoGB <gustavo.gobetti98@gmail.com>
2019-10-20 10:20:06 -0300

    Metade das funcoes para fastApi, bug para rodar o uvicorn

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import pymysql
 
 app = FastAPI()
 
-
-@app.post("/usuarios/{usuario_id}")
+#Adiciona Usuario
+@app.post("/usuarios/{id_usuario}")
 def adiciona_usuario(conn, nome, email, cidade):
     with conn.cursor() as cursor:
         try:
@@ -13,8 +13,8 @@ def adiciona_usuario(conn, nome, email, cidade):
         except pymysql.err.IntegrityError as e:
             raise ValueError(f'Não posso inserir {nome} na tabela perigo')
 
-
-@app.get("/usuarios/{usuario_id}")
+#Encontra Usuario
+@app.get("/usuarios/{id_usuario}")
 def acha_usuario(conn, nome):
     with conn.cursor() as cursor:
         cursor.execute('SELECT id_usuario FROM usuario WHERE nome = %s', (nome))
@@ -23,23 +23,24 @@ def acha_usuario(conn, nome):
             return res[0]
         else:
             return None		
-
-@app.put("/usuarios/{usuario_id}")
+#Muda nome do Usuario
+@app.put("/usuarios/{id_usuario}")
 def muda_nome_usuario(conn, id_usuario, novo_nome):
     with conn.cursor() as cursor:
         try:
             cursor.execute('UPDATE usuario SET nome=%s where id_usuario=%s', (novo_nome, id_usuario))
         except pymysql.err.IntegrityError as e:
             raise ValueError(f'Não posso alterar nome do id {id} para {novo_nome} na tabela usuario')
-
-@app.delete("/usuario/{usuario_id}")
+#Remove usuario
+@app.delete("/usuario/{id_usuario}")
 def remove_usuario(conn, id_usuario):
     with conn.cursor() as cursor:
         try:
         	cursor.execute('UPDATE usuario SET ativo = False WHERE id_usuario=%s', (id_usuario))
         except pymysql.err.IntegrityError as e:
             raise ValueError(f'Não posso remover usuario de id {id} na tabela usuario')
-@app.get("/usuario/{usuario_id}")
+#Lista usuarios
+@app.get("/usuario/{id_usuario}")
 def lista_usuarios(conn):
     with conn.cursor() as cursor:
         cursor.execute('SELECT id_usuario from usuario')
@@ -47,6 +48,8 @@ def lista_usuarios(conn):
         usuarios = tuple(x[0] for x in res)
         return usuarios
 
+#Checa se usuario existe        
+@app.get("/usuario/{id_usuario}")
 def checa_ativo(conn, id_usuario):
     with conn.cursor() as cursor:
         cursor.execute('SELECT ativo FROM usuario WHERE id_usuario = %s', (id_usuario))
@@ -55,3 +58,40 @@ def checa_ativo(conn, id_usuario):
             return res[0] #Retorna se está 1 ou 0
         else:
             return -1
+
+#Adiciona Passaros
+@app.post("/passaros/{id_passaro}")
+def adiciona_passaro(conn, especie):
+    with conn.cursor() as cursor:
+        try:
+            cursor.execute('INSERT INTO passaro (especie) VALUES (%s)', (especie))
+        except pymysql.err.IntegrityError as e:
+            raise ValueError(f'Não posso inserir {especie} na tabela comida')
+#Encontra um passaro especifico
+@app.get("/passaros/{id_passaro}")
+def acha_passaro(conn, especie):
+    with conn.cursor() as cursor:
+        cursor.execute('SELECT id_passaro FROM passaro WHERE especie = %s', (especie))
+        res = cursor.fetchone()
+        if res:
+            return res[0]
+        else:
+            return None
+#Deleta passaros            
+@app.delete("/passaros/{id_passaro}")
+def remove_passaro(conn, id_passaro):
+    with conn.cursor() as cursor:
+        cursor.execute('DELETE FROM passaro WHERE id_passaro=%s', (id_passaro))
+
+#Lista todos os passaros
+@app.get("/passaros/{id_passaro}")
+def lista_passaros(conn):
+    with conn.cursor() as cursor:
+        cursor.execute('SELECT id_passaro from passaro')
+        res = cursor.fetchall()
+        passaros = tuple(x[0] for x in res)
+        return passaros
+
+
+
+
